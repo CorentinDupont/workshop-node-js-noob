b346fb03bf69bb23492166aa7fc709dd54edbe88
CorentinDupont <dupont.corentin44@gmail.com>
2017-11-21 16:26:29 +0100

    exercice 4

--- src/exercise4.js
+++ src/exercise4.js
@@ -2,7 +2,7 @@ exports.run = function(input) {
   console.log("Exercise #4");
 
   // YOUR CODE IS WELCOME HERE
-
+  return `Beetlejuice, Beetlejuice, Beetlejuice`;
   /** STRICTLY FORBIDDEN TO CHANGE THIS CODE */
   return input(3, "Beetlejuice", repeat);
   /** */
