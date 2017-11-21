2a2a3df696e3a936623c6e53d1c4b8162df59672
CorentinDupont <dupont.corentin44@gmail.com>
2017-11-21 16:58:16 +0100

    exercice5

--- src/exercise5.js
+++ src/exercise5.js
@@ -1,3 +1,15 @@
 exports.run = function(input) {
   console.log("Exercise #5");
+
+  var fs = require('fs');
+
+  input.showDone = (number) => console.log(`Project contains ${number} file(s) of tests`);
+
+  input.showInProgress();
+  fs.readdir(input.directory, function(err, data){
+    if(err){
+      input.showError();
+    }
+    input.showDone(data.length);
+  });
 };
