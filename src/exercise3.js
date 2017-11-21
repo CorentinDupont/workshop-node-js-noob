a8f72ddc7699cd010512510fe5fa7efe735993ac
CorentinDupont <dupont.corentin44@gmail.com>
2017-11-21 16:19:26 +0100

    exercice3.js

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,3 +1,9 @@
 exports.run = function(input) {
-  console.log("Exercise #3");
+  var multipliedNum = 1;
+  input.forEach(obj => {
+    if(obj.multiply){
+      multipliedNum*=obj.value;
+    }
+  });
+  return multipliedNum;
 };
